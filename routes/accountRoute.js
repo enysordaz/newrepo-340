c179727d069bc6051ddd4919c3549fc8a1a792de
Enys Ordaz <ord23002@byui.edu>
2025-06-06 18:53:00 -0600

    New changes and W4 activities

---
+++ routes/accountRoute.js
@@ -0,0 +1,22 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+//Double check this
+const utilities = require("../utilities/")
+const accountController = require("../controllers/accountController");
+
+//Error route
+const errorController = require('../controllers/errorController');
+
+// Route to build inventory by classification view
+router.get("/detail/:id", accountController.getMyAccount );
+
+// GET /inventory/:id (for example)
+//router.get('/detail/:id', invController.getCarById);
+
+// New route to intentionally trigger 500 error
+router.get('/trigger-error', errorController.throwServerError);
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,9 +13,35 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const utilities = require('./utilities/'); // or './utilities/index' if it's in a folder -- fixed bug
 const inventoryRoute = require('./routes/inventoryRoute')
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require('./routes/accountRoute')
 
+//error Route
 const errorRoutes = require('./routes/inventoryRoute');
 app.use('/', errorRoutes); // or '/inventory' if scoped
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 /* ***********************
  * View Engine and Templates
@@ -34,6 +60,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Account route
+app.use("/account", accountRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
